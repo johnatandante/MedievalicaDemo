35ff92141ecabd6096cca634e87ca6c565927a7c
Dante Del Favero <dante.delfavero@gmail.com>
2017-05-21 16:07:27 +0200

    Code to connect to Localhost

--- src/Medievalica.Client/Program.cs
+++ src/Medievalica.Client/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
     public class Program
     {
+        static ClientWebSocket client;
+
+        /// <summary>
+        /// https://radu-matei.github.io/blog/aspnet-core-websockets-middleware/
+        /// </summary>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try{
+            RunWebSockets().GetAwaiter().GetResult();
+            }catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
         }
+
+        private static async Task RunWebSockets()
+        {
+            client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri("ws://localhost:5000/test"), CancellationToken.None);
+
+            Console.WriteLine("Connected!");
+
+            bool endUpNow = false;
+            string command = string.Empty;
+            while (!endUpNow)
+            {
+                command = System.Console.ReadLine();
+
+                if(command != "quit"){
+                    endUpNow = true;
+                    continue;
+                } else
+                {
+                    await SendMessage(command);
+                    System.Threading.Thread.Sleep(500);
+                }
+
+            }
+            
+            await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Premi un tasto per proseguire...");
+            Console.ReadKey();
+        }
+
+        private static async Task SendMessage(string message){
+
+            var bytes = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+                
+        }
+
     }
 }
--- src/Medievalica.Server/Program.cs
+++ src/Medievalica.Server/Program.cs
@@ -4,6 +4,11 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        /// <summary>
+        /// Look at 
+        /// https://radu-matei.github.io/blog/aspnet-core-websockets-middleware/
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
